1201014c2777868f551f33855d5b24da488304b4
cgomezrando <cgomezrando@gmail.com>
2026-08-07 16:02:22 +0200

    Add files via upload

--- lib/pages/splash_page/splash_page_model.dart
+++ lib/pages/splash_page/splash_page_model.dart
@@ -1,9 +1,12 @@
+import '/auth/firebase_auth/auth_util.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/index.dart';
 import 'splash_page_widget.dart' show SplashPageWidget;
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
--- lib/pages/splash_page/splash_page_widget.dart
+++ lib/pages/splash_page/splash_page_widget.dart
@@ -1,8 +1,11 @@
+import '/auth/firebase_auth/auth_util.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/index.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'splash_page_model.dart';
@@ -27,6 +30,17 @@ class _SplashPageWidgetState extends State<SplashPageWidget> {
   void initState() {
     super.initState();
     _model = createModel(context, () => SplashPageModel());
+
+    // On page load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      GoRouter.of(context).prepareAuthEvent();
+      final user = await authManager.signInAnonymously(context);
+      if (user == null) {
+        return;
+      }
+
+      context.pushNamedAuth(HomePageWidget.routeName, context.mounted);
+    });
   }
 
   @override
